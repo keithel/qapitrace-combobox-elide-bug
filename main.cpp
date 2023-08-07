0530e0ed37d9ed27b33925e76d6d4cd3f9ad1a7c
Keith Kyzivat <keith.kyzivat@qt.io>
2023-08-07 13:32:16 -0400

    Reduce C++ impl to minimal reproducing example
    This bug occurs when building against Qt 5.15.3, 5.15.7, and
    5.15.14, and presumably Qt releases between those versions. Those just
    were the ones I tested.
    
    This bug does not occur when building against Qt 5.15.2 or 6.5.1.
    Given that Qt 5.15.14 is the latest Qt 5.15 commercial release, it would
    seem that the bug was introduced in Qt 5.15.3, but fixed sometime on or
    after Qt 6.0.0.

--- main.cpp
+++ main.cpp
@@ -1,55 +1,10 @@
-#include "mainwindow.h"
-#include "./ui_retracerdialog.h"
-
 #include <QApplication>
 #include <QComboBox>
-#include <QTimer>
-
-class ComboBox : public QComboBox
-{
-public:
-    ComboBox() {
-        timer.setSingleShot(true);
-        connect(&timer, &QTimer::timeout, this, [this](){ showPopup(); });
-    }
-    void showEvent(QShowEvent *e) {
-        if(!mFirstTimeShown)
-        {
-            timer.start(100);
-            mFirstTimeShown = true;
-        }
-    }
-
-private:
-    QTimer timer;
-    bool mFirstTimeShown = false;
-};
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-//    MainWindow w;
-//    w.show();
-
-//    QDialog dlg;
-//    Ui_RetracerDialog dlgUi;
-//    dlgUi.setupUi(&dlg);
-
-//    dlgUi.doubleBufferingCB->setChecked(true);
-//    dlgUi.errorCheckCB->setChecked(true);
-//    dlgUi.singlethreadCB->setChecked(false);
-//    dlgUi.coreProfileCB->setChecked(false);
-
-//    dlgUi.queryHandlingSelector->addItem("Skip");
-//    dlgUi.queryHandlingSelector->addItem("Run");
-//    dlgUi.queryHandlingSelector->addItem("Run & Check");
-//    //dlgUi.queryHandlingSelector->setMinimumContentsLength(11);
-//    dlgUi.queryHandlingSelector->setCurrentIndex(0);
-//    dlgUi.queryCheckReportThreshold->setValue(0);
-
-//    dlg.exec();
-
-    ComboBox combo;
+    QComboBox combo;
     combo.addItem("Skip");
     combo.addItem("Run");
     combo.addItem("Run & Check");
@@ -57,5 +12,3 @@ int main(int argc, char *argv[])
 
     return a.exec();
 }
-
-#include "main.moc"
